e64ebb346e8fe0a1b1c7c39c0850945643690615
Todd Morningstar <techarchist@gmail.com>
2016-08-08 15:31:31 -0400

    Add initial code lookup resources.

---
+++ app/controllers/codes_controller.rb
@@ -0,0 +1,10 @@
+class CodesController < ApplicationController
+  def index
+  end
+
+  def description
+  end
+
+  def submit_code
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+  # Diagnosis Code Lookups
+  get  'codes/index', controller: :codes, action: :index, as: :codes
+  get  'codes/description', controller: :codes, action: :description
+  post 'codes/submit_code', controller: :codes, action: :submit_code
+
   root to: 'home#index'
 end
