82bb4b6e0a084ab6f3bd578019df53bc3835f223
Todd Morningstar <techarchist@gmail.com>
2016-08-08 14:15:19 -0400

    Add root route and 'home' page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  # Handle HTML <head><title> content.
+  def title(page_title)
+    content_for(:title) { page_title.to_s }
+  end
 end
---
+++ config/initializers/01_application_consts.rb
@@ -0,0 +1,1 @@
+DEFAULT_TITLE = 'MedlinePlus Connect API Gem Demo'.freeze
