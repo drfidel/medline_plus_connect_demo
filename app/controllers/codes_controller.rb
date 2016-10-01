6a73f65256d44357461ed4badfa8b22a5c789d87
Todd Morningstar <techarchist@gmail.com>
2016-10-01 08:47:44 -0400

    Update front end to display response from ICD-10 code lookup.
    * New global styles to handle hidden/shown elements.
     * Bootstrap 4 drops support for hide/show.
    * CodesController handles action to get ICD-10 code descriptions.
    * New views to handle asynch display of descriptions.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,10 +1,18 @@
 class CodesController < ApplicationController
+
   def index
   end
 
   def description
+    @code_description = CodeDescription.new
   end
 
   def submit_code
+    @code_description = CodeDescription.new code: params[:diagnosis_code]
+    @code_description.fetch_code_description!
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
