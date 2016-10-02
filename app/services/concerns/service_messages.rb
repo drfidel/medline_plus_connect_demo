d85a8ef37fcbfbbc353d31927c3d0d03ab576b0d
Todd Morningstar <techarchist@gmail.com>
2016-10-02 11:08:34 -0400

    Improve Exception/Error handling between Controller and Service.

---
+++ app/services/concerns/service_messages.rb
@@ -0,0 +1,5 @@
+module ServiceMessages
+  MESSAGE_SUCCESS   = 'Success'.freeze
+  MESSAGE_INVALID   = 'Invalid Diagnosis Code.'.freeze
+  ERROR_NO_RESPONSE = 'Unable to reach diagnosis code client.'.freeze
+end
--- app/services/diagnosis_code_description_service.rb
+++ app/services/diagnosis_code_description_service.rb
@@ -4,7 +4,10 @@
 #  e.g., `response = DiagnosisCodeDescriptorService.perform('J01.01')`.
 # See https://github.com/envato/aldous for full documentation.
 class DiagnosisCodeDescriptionService < Aldous::Service
+  include ServiceMessages
+
   attr_reader :diagnosis_code
+  attr_reader :message
 
   def initialize(diagnosis_code)
     @diagnosis_code = diagnosis_code
@@ -13,7 +16,7 @@ class DiagnosisCodeDescriptionService < Aldous::Service
   # Default value to return if no data is provided to the `Result` Data
   #  Transfer Object, or if an exception is raised during the `#perform` call.
   def default_result_data
-    { descriptions: @diagnosis_code }
+    { descriptions: @diagnosis_code, message: MESSAGE_INVALID }
   end
 
   # Makes ICD-10 API call, and returns result payload in `Result` Data Transfer
@@ -24,12 +27,10 @@ class DiagnosisCodeDescriptionService < Aldous::Service
 
     if client_response
       if client_response['feed'].present? && client_response['feed']['entry'].present?
-        Result::Success.new descriptions: client_response['feed']['entry']
-      else
-        Result::Failure.new errors: 'Invalid Diagnosis Code.'
+        Result::Success.new descriptions: client_response['feed']['entry'], message: MESSAGE_SUCCESS
       end
     else
-      Result::Failure.new errors: 'Unable to reach diagnosis code client.'
+      Result::Failure.new errors: ERROR_NO_RESPONSE
     end
   end
 end
