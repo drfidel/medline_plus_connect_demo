54289034a9737e38e9876c4bf487d093a84951ed
Todd Morningstar <techarchist@gmail.com>
2016-10-02 17:21:58 -0400

    Do not allow public setting of CodeDescription#description_data.

--- app/models/code_description.rb
+++ app/models/code_description.rb
@@ -1,7 +1,12 @@
 # Handles retrieval of medical descriptions associated with a diagnosis code.
+#  E.g., "S11.012" or "J01.01"
 class CodeDescription
   include ActiveModel::Model
   include DiagnosisCodeHelper
   
-  attr_accessor :code, :description_data
+  attr_accessor :code
+  attr_reader   :description_data
+
+  private
+    attr_writer :description_data
 end
