02b7d2c9027bdc40e8ce9cdcfd2aa2073b996bf5
Todd Morningstar <techarchist@gmail.com>
2016-10-12 11:56:42 -0400

    Add initial spec for CodeDescription.

---
+++ spec/models/code_description_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe CodeDescription do
+
+  it { is_a? ActiveModel::Model }
+
+  context 'attributes' do
+    let(:cd)   { CodeDescription.new }
+    let(:data) { { hash: 'browns' } }
+    let(:code) { 'J01.01' }
+
+    describe '#description_data=' do
+      it 'should not be publically writable' do
+        expect{ cd.description_data = data }.to raise_error(
+          NoMethodError, /private method `description_data=' called/)
+      end
+
+      it 'should be publically readable' do
+        allow(cd).to receive(:description_data) { data }
+
+        expect{ cd.description_data }.to_not raise_error
+        expect(cd.description_data).to eq(data)
+      end
+    end
+
+    describe '#code' do
+      it 'should be publically writable' do
+        expect{ cd.code = code }.to_not raise_error
+      end
+
+      it 'should be publically readable' do
+        allow(cd).to receive(:code) { code }
+
+        expect{ cd.code }.to_not raise_error
+        expect(cd.code).to eq(code)
+      end
+    end
+  end
+
+end
